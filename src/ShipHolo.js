33a0973ba34b9e07e23e7c2400a2329ed730cc6b
tconey23 <145934889+tconey23@users.noreply.github.com>
2024-08-29 17:23:37 -0600

    further expansion of location accordion

--- src/ShipHolo.js
+++ src/ShipHolo.js
@@ -48,19 +48,43 @@ const ShipHolo = ({ shipUrl }) => {
     const [holoUrl, setHoloUrl] = useState(null);
     const controlsRef = useRef();
     const suspenseRef = useRef()
+    const [environment, setEnvironment] = useState()
+    const [devOrProd, setDevOrProd] = useState()
 
     const newMaterial = new THREE.MeshPhysicalMaterial({
         color: '#00a2ff',
     });
-    
+
+    const vercel = 'https://spacetrucking.vercel.app/spaceTrucking/api/'
+    const dev = 'http://localhost:5001/'
+
     useEffect(() => {
-        if (shipUrl) {
+
+        setEnvironment(window.location.hostname)
+        
+        // console.log(window.location.hostname)
+    }, [])
+
+    useEffect(() => {
+        console.log(environment)
+
+        if(environment === 'localhost'){
+            setDevOrProd(dev)
+        } else {
+            setDevOrProd(vercel)
+        }
+
+    }, [environment])
+
+
+    useEffect(() => {
+        if (shipUrl && devOrProd) {
             const fetchGltf = async () => {
                 try {
-                    const response = await fetch(`http://localhost:5001/fetch-gltf?url=${encodeURIComponent(shipUrl)}`);
+                    const response = await fetch(`${devOrProd}fetch-gltf?url=${encodeURIComponent(shipUrl)}`);
                     if (response.ok) {
                         const filename = new URL(shipUrl).pathname.split('/').pop();
-                        setHoloUrl(`http://localhost:5001/downloads/${filename}`);
+                        setHoloUrl(`${devOrProd}downloads/${filename}`);
                     } else {
                         console.error('Failed to fetch GLTF:', response.statusText);
                     }
@@ -76,7 +100,7 @@ const ShipHolo = ({ shipUrl }) => {
     
     const emptyDownloadsFolder = async () => {
         try {
-            const response = await fetch(`http://localhost:5001/empty-downloads`, {
+            const response = await fetch(`${devOrProd}empty-downloads`, {
                 method: 'DELETE',
             });
     
